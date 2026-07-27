4e68aecc495583144ac6cea8ce17eef4e670562b
mounika <dinesh.cd15@gmail.com>
2026-07-28 00:19:15 +0200

    add bank application

--- packer/scripts/install_monitoring.sh
+++ packer/scripts/install_monitoring.sh
@@ -21,8 +21,6 @@ sudo mkdir -p /etc/prometheus /var/lib/prometheus
 # Install Prometheus binaries
 sudo cp prometheus-${PROM_VERSION}.linux-amd64/prometheus /usr/local/bin/
 sudo cp prometheus-${PROM_VERSION}.linux-amd64/promtool /usr/local/bin/
-sudo cp -r prometheus-${PROM_VERSION}.linux-amd64/consoles /etc/prometheus
-sudo cp -r prometheus-${PROM_VERSION}.linux-amd64/console_libraries /etc/prometheus
 
 # Set permissions
 sudo chown -R prometheus:prometheus /etc/prometheus
@@ -43,10 +41,7 @@ Type=simple
 ExecStart=/usr/local/bin/prometheus \
 --config.file=/etc/prometheus/prometheus.yml \
 --web.listen-address=0.0.0.0:9090 \
---storage.tsdb.path=/var/lib/prometheus \
---web.console.templates=/etc/prometheus/consoles \
---web.console.libraries=/etc/prometheus/console_libraries
-
+--storage.tsdb.path=/var/lib/prometheus
 Restart=always
 
 [Install]
