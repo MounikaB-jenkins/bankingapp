956aad913c7122502c33f9936101af7f590ee1be
mounika <dinesh.cd15@gmail.com>
2026-08-03 18:18:23 +0200

    issues

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,7 +67,7 @@ resource "aws_security_group" "alb" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = var.trusted_ip_cidr
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
