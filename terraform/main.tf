ca531673490251ae174aa227066af1117b359814
mounika <dinesh.cd15@gmail.com>
2026-08-21 01:18:20 +0200

    issues

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,8 +23,10 @@ data "aws_availability_zones" "available" {
 
 # Optional VPC/subnet creation when migrating to a new account
 resource "aws_vpc" "default" {
-  count     = var.create_vpc ? 1 : 0
-  cidr_block = var.vpc_cidr
+  count             = var.create_vpc ? 1 : 0
+  cidr_block        = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
   tags = {
     Name    = "bankingapp-vpc"
     Project = "BankingApp"
