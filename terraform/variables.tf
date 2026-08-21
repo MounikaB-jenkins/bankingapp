a05085e9f93b93229eb2f0658cd1ff2f8074f67e
MounikaB-jenkins <mounika.b5693@gmail.com>
2026-08-21 06:23:53 +0200

    Set default of create_vpc to true
    Changed default value of create_vpc variable to true.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,7 +25,7 @@ variable "subnet_ids" {
 variable "create_vpc" {
   description = "When true, Terraform will create a new VPC and subnets instead of using existing ones"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "vpc_cidr" {
